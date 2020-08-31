8020bc6404ab9eb2156d93360d10f161994b6e64
Rafael <akafael@users.noreply.github.com>
2020-08-30 23:21:35 -0300

    Modify Hello World example to minimal server app

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+/**
+ * Server Application
+ */
+
+import express from 'express';
+
+const app = express();
+
+
+// Add 
+app.get('/users', (request, response) => {
+    console.log("Hello HTTP");
+    return response.send("Hello World!");
+})
+
+// List to HTTP Requests from Port 3333
+app.listen(3333);
